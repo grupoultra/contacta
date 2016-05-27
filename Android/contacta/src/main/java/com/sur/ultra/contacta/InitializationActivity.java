58fbae5944029f0f247644ff93df56e439017069
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2016-05-27 15:24:35 -0400

    Adapter para el spinner

--- Android/contacta/src/main/java/com/sur/ultra/contacta/InitializationActivity.java
+++ Android/contacta/src/main/java/com/sur/ultra/contacta/InitializationActivity.java
@@ -28,6 +28,7 @@ public class InitializationActivity extends AppCompatActivity {
         // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
 
     public void newInitialization(View view){
