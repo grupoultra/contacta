abe0ce4a8578e3511a3d25a85a4ed0cd0054c48d
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2016-06-01 13:58:20 -0400

    Implementacion de providers con hashmap

--- Android/contacta/src/main/java/com/sur/ultra/contacta/Models/Providers.java
+++ Android/contacta/src/main/java/com/sur/ultra/contacta/Models/Providers.java
@@ -1,31 +1,43 @@
 package com.sur.ultra.contacta.Models;
 
+import com.sur.ultra.contacta.R;
+
 import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 
 /**
  * Created by alexis on 5/31/16.
  */
 public class Providers {
 
-    protected ArrayList<Provider> providers;
+    protected HashMap<String, Provider> providers;
 
     public Providers() {
-        this.providers = new ArrayList<Provider>();
+        this.providers = new HashMap<String, Provider>();
 
-        this.addOne("1", "Banco Mercantil", "Este es Banco Mercantil", "avatar");
-        this.addOne("2", "Cantv", "Este es Cantv", "avatar");
-        this.addOne("3", "Movistar de Venezuela", "Este es Movistar de Venezuela", "avatar");
-        this.addOne("4", "GMVV", "Este es GMVV", "avatar");
-        this.addOne("5", "LaIguana.TV", "Este es LaIguana.TV", "avatar");
-        this.addOne("6", "Banco Provincial", "Este es Banco Provincial", "avatar");
+        String dummyInfo  = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sed congue justo, vel pulvinar nunc. Morbi ultricies porta dolor a auctor. Mauris vel lorem imperdiet, sagittis leo at, faucibus justo. Vestibulum id lacus lorem. Proin ipsum sem, dignissim vel luctus porttitor, condimentum vel purus. Integer id efficitur arcu, quis venenatis neque. Proin metus dui, mattis ac ullamcorper ac, vehicula ut metus. Aenean mattis, nunc malesuada suscipit ultrices, leo diam porta nulla, quis iaculis elit augue vel odio. Sed dapibus augue et mi vulputate, auctor malesuada dui suscipit. Integer id urna fermentum, sollicitudin orci a, eleifend est. Quisque suscipit eget velit eget hendrerit. Fusce mi leo, tempus porta eleifend sed, aliquet sit amet eros. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Phasellus et dolor lacus. Integer hendrerit, tellus ac posuere congue, augue augue ultrices risus, quis convallis justo diam et urna. \nMaecenas congue, odio sed sodales dictum, diam risus efficitur nisl, ac venenatis nisl arcu et dolor. Etiam vel tristique turpis. Pellentesque lectus felis, porttitor at porttitor ut, cursus id mauris. Vivamus elit augue, porttitor vel vulputate nec, varius feugiat est. Vivamus et egestas diam, vel sollicitudin lorem. Vivamus diam libero, egestas et semper quis, iaculis sit amet enim. In hac habitasse platea dictumst. Proin pretium lacus ac ullamcorper finibus";
+
+        this.addOne("1", "Banco Mercantil", dummyInfo, "avatar");
+        this.addOne("2", "Cantv", dummyInfo, "avatar");
+        this.addOne("3", "Movistar de Venezuela", dummyInfo, "avatar");
+        this.addOne("4", "GMVV", dummyInfo, "avatar");
+        this.addOne("5", "LaIguana.TV", dummyInfo, "avatar");
+        this.addOne("6", "Banco Provincial", dummyInfo, "avatar");
     }
 
-    public ArrayList<Provider> getAll() {
-        return providers;
+    public Provider getOne(String id){
+        return this.providers.get(id);
+    }
+
+    public ArrayList<Provider> getAllList() {
+        return new ArrayList<Provider>(this.providers.values());
     }
 
     public void addOne(String id, String name, String info, String avatar) {
-        this.providers.add(new Provider(id, name, info, avatar));
+        this.providers.put(id, new Provider(id, name, info, avatar));
+    }
+
+    public void removeOne(String id){
+        this.providers.remove(id);
     }
 }
