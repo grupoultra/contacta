6e98b1f418be014461460200b9b199d735f8347c
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2016-06-03 10:27:30 -0400

    Recuperando las clases necesarias para integrar GCM

--- Android/contacta/src/main/java/com/sur/ultra/contacta/FragmentoNoticias.java
+++ Android/contacta/src/main/java/com/sur/ultra/contacta/FragmentoNoticias.java
@@ -7,6 +7,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.sur.ultra.contacta.Models.Message;
+
 /**
  * Created by alexis on 6/2/16.
  */
@@ -26,13 +28,13 @@ public class FragmentoNoticias extends android.support.v4.app.Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragmento_grupo_items, container, false);
+        View view = inflater.inflate(R.layout.fragments_recycler_view, container, false);
 
         RecyclerView reciclador = (RecyclerView)view.findViewById(R.id.reciclador);
         linearLayout = new LinearLayoutManager(getActivity());
         reciclador.setLayoutManager(linearLayout);
 
-        AdaptadorMensajes adaptador = new AdaptadorMensajes();
+        MessageAdapter adaptador = new MessageAdapter(Message.NEWS);
         reciclador.setAdapter(adaptador);
         reciclador.addItemDecoration(new DecoracionLineaDivisoria(getActivity()));
 
