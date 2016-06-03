63280e0f13a7430f65aa4c60d1a81f61104ab9b8
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2016-06-03 18:20:19 -0400

    Modificaciones minimas

--- Android/contacta/src/main/java/com/sur/ultra/contacta/Fragments/ProviderDetailFragment.java
+++ Android/contacta/src/main/java/com/sur/ultra/contacta/Fragments/ProviderDetailFragment.java
@@ -1,11 +1,14 @@
 package com.sur.ultra.contacta.Fragments;
 
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 import com.sur.ultra.contacta.R;
 
