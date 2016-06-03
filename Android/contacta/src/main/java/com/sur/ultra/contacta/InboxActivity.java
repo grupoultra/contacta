e7b00a0df7653820f196cc0713177e93760d2444
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2016-06-03 10:29:23 -0400

    Integracion de fragment con secciones de inbox y proveedores

---
+++ Android/contacta/src/main/java/com/sur/ultra/contacta/InboxActivity.java
@@ -0,0 +1,136 @@
+package com.sur.ultra.contacta;
+
+import android.content.DialogInterface;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class InboxActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inbox);
+
+        agregarToolbar();
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+
+        if (navigationView != null) {
+            prepararDrawer(navigationView);
+            // Seleccionar item por defecto
+            seleccionarItem(navigationView.getMenu().getItem(0));
+        }
+    }
+
+    private void agregarToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        final ActionBar ab = getSupportActionBar();
+        if (ab != null) {
+            // Poner ícono del drawer toggle
+            ab.setHomeAsUpIndicator(R.drawable.drawer_toggle);
+            ab.setDisplayHomeAsUpEnabled(true);
+        }
+
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+    private void prepararDrawer(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        menuItem.setChecked(true);
+                        seleccionarItem(menuItem);
+                        drawerLayout.closeDrawers();
+                        return true;
+                    }
+                });
+
+    }
+
+    private void seleccionarItem(MenuItem itemDrawer) {
+        Fragment fragmentoGenerico = null;
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        switch (itemDrawer.getItemId()) {
+            case R.id.item_inicio:
+                fragmentoGenerico = new InboxFragment();
+                break;
+            case R.id.item_cuenta:
+                fragmentoGenerico = new ProvidersFragment();
+                break;
+        }
+        if (fragmentoGenerico != null) {
+            fragmentManager
+                    .beginTransaction()
+                    .replace(R.id.contenedor_principal, fragmentoGenerico)
+                    .commit();
+        }
+
+        // Setear título actual
+        setTitle(itemDrawer.getTitle());
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.messages, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+
+            case R.id.action_sort:
+                final ArrayList mSelectedItems = new ArrayList();  // Where we track the selected items
+
+                new AlertDialog.Builder(InboxActivity.this)
+                        .setTitle("Ordenar")
+                        .setPositiveButton("Por proveedor", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(InboxActivity.this, "Ordenado por proveedor", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .setNegativeButton("Por fecha", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(InboxActivity.this, "Ordenado por fecha", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ Android/contacta/src/main/java/com/sur/ultra/contacta/MessageAdapter.java
@@ -0,0 +1,57 @@
+package com.sur.ultra.contacta;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.sur.ultra.contacta.Models.Message;
+
+import java.util.List;
+
+/**
+ * Created by alexis on 6/2/16.
+ */
+public class MessageAdapter
+        extends RecyclerView.Adapter<MessageAdapter.ViewHolder> {
+
+    private final List<Message> items;
+
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        // Campos respectivos de un item
+        public TextView messageSummary;
+        public TextView author;
+
+        public ViewHolder(View v) {
+            super(v);
+            messageSummary = (TextView) v.findViewById(R.id.text_messageSummary);
+            author = (TextView) v.findViewById(R.id.text_author);
+        }
+    }
+
+    public MessageAdapter(List<Message> items) { this.items = items; }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.item_message_list, viewGroup, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, int i) {
+        Message item = items.get(i);
+        viewHolder.messageSummary.setText(item.messageSummary);
+        viewHolder.author.setText(item.author);
+    }
+
+}
+
+
