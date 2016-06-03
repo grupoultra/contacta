03519bd189a2f476e1d8f4d84236786020c5fc57
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2016-06-03 11:47:20 -0400

    activando el spinner

--- Android/contacta/src/main/java/com/sur/ultra/contacta/InitializationFragment.java
+++ Android/contacta/src/main/java/com/sur/ultra/contacta/InitializationFragment.java
@@ -9,7 +9,9 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 
@@ -38,6 +40,16 @@ public class InitializationFragment extends Fragment {
                 Toast.makeText(getActivity(), "Comenzar proceso de inicializacion", Toast.LENGTH_SHORT).show();
             }
         });
+
+        Spinner spinner = (Spinner) view.findViewById(R.id.spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
+                R.array.countries_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
         return view;
     }
 }
