275c5e7cb50d15dd9c9c3a3eb8cbf096b9d895c5
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2016-06-03 16:18:04 -0400

    Habilitando onClick para mensaejs

--- Android/contacta/src/main/java/com/sur/ultra/contacta/Adapters/MessageAdapter.java
+++ Android/contacta/src/main/java/com/sur/ultra/contacta/Adapters/MessageAdapter.java
@@ -1,14 +1,23 @@
 package com.sur.ultra.contacta.Adapters;
 
+import android.app.Activity;
+import android.content.Context;
+import android.app.FragmentManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.sur.ultra.contacta.Fragments.MessagesFragment;
+import com.sur.ultra.contacta.Fragments.NewsDetailFragment;
+import com.sur.ultra.contacta.Fragments.NewsFragment;
 import com.sur.ultra.contacta.Models.Message;
 import com.sur.ultra.contacta.R;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,21 +27,45 @@ public class MessageAdapter
         extends RecyclerView.Adapter<MessageAdapter.ViewHolder> {
 
     private final List<Message> items;
+    private Context ctx;
+    private MessagesFragment.OnHeadlineSelectedListener mCallback;
+
+    public MessageAdapter(List<Message> items, Context ctx, MessagesFragment.OnHeadlineSelectedListener mCallback) {
+        this.items = items;
+        this.ctx = ctx;
+        this.mCallback = mCallback;
+    }
 
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         // Campos respectivos de un item
         public TextView messageSummary;
         public TextView author;
+        private List<Message> messages = new ArrayList<Message>();
+        private Context ctx;
+        private MessagesFragment.OnHeadlineSelectedListener mCallback;
 
-        public ViewHolder(View v) {
+
+        public ViewHolder(View v, Context ctx, List<Message> messages, MessagesFragment.OnHeadlineSelectedListener mCallback) {
             super(v);
+            this.messages = messages;
+            this.ctx = ctx;
+            this.mCallback = mCallback;
+
+            v.setOnClickListener(this);
+
             messageSummary = (TextView) v.findViewById(R.id.text_messageSummary);
             author = (TextView) v.findViewById(R.id.text_author);
         }
-    }
 
-    public MessageAdapter(List<Message> items) { this.items = items; }
+        @Override
+        public void onClick(View v) {
+            int position = getAdapterPosition();
+            mCallback.onArticleSelected(3);
+//            Log.d("mydebugger", String.valueOf(v.getContext().getClass()));
+//            Toast.makeText(v.getContext(), "Item seleccionado", Toast.LENGTH_SHORT).show();
+        }
+    }
 
     @Override
     public int getItemCount() {
@@ -43,7 +76,7 @@ public class MessageAdapter
     public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
         View v = LayoutInflater.from(viewGroup.getContext())
                 .inflate(R.layout.item_message_list, viewGroup, false);
-        return new ViewHolder(v);
+        return new ViewHolder(v,this.ctx, this.items, mCallback );
     }
 
     @Override
