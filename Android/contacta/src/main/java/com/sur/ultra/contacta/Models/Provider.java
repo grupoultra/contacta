b7d89ac4e9bcc1f15e24526ebe69b0bde6dc04e2
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2016-06-17 14:44:43 -0400

    Anadiendo getters y setter a Provider

--- Android/contacta/src/main/java/com/sur/ultra/contacta/Models/Provider.java
+++ Android/contacta/src/main/java/com/sur/ultra/contacta/Models/Provider.java
@@ -13,6 +13,38 @@ public class Provider {
     public String info;
     public String avatar;
 
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public Provider(int id, String name, String info, String avatar) {
         this.id = id;
         this.name = name;
